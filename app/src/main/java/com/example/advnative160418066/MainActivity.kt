f6ac869965203a7a0a22eb7bf529b72c6b3feca7
sameh4d4 <71703749+sameh4d4@users.noreply.github.com>
2021-08-12 14:21:05 +0700

    add name

--- app/src/main/java/com/example/advnative160418066/MainActivity.kt
+++ app/src/main/java/com/example/advnative160418066/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.advnative160418066
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+val nama:String="Nicky"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Wellcome to Android", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Wellcome to Android $nama", Toast.LENGTH_SHORT).show()
     }
 }
